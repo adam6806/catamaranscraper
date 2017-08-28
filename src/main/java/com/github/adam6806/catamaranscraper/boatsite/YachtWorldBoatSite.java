8dc05be3c66f14ff9805c3f235d7ccd9fe92db6e
Adam <asmith0935@gmail.com>
2017-08-27 19:08:02 -0600

    Fixed stuff I broke

--- src/main/java/com/github/adam6806/catamaranscraper/boatsite/YachtWorldBoatSite.java
+++ src/main/java/com/github/adam6806/catamaranscraper/boatsite/YachtWorldBoatSite.java
@@ -18,9 +18,10 @@ import java.util.*;
 
 public class YachtWorldBoatSite implements BoatSite {
 
-    private List<BoatEntity> entities;
     private static final String URL = "http://www.yachtworld.com/core/listing/cache/searchResults.jsp?toPrice=175000&fromPrice=25000&enid=101&Ntk=boatsEN&type=%28Sail%29+Catamaran&searchtype=advancedsearch&hmid=0&sm=3&enid=0&cit=true&luom=126&currencyid=100&boatsAddedSelected=-1&ftid=0&slim=quick&No=0&rid=100&rid=104&rid=105&rid=107&rid=112&rid=115&rid=125&fracts=1&ps=2000&Ns=PBoat_sortByPriceDesc|1";
     private static final String MAIN_URL = "http://www.yachtworld.com";
+    private Log logger = new SimpleLog(YachtWorldBoatSite.class.getName());
+    private List<BoatEntity> entities;
     private Driver driver;
 
     public static void main(String[] args) {
@@ -229,17 +230,17 @@ public class YachtWorldBoatSite implements BoatSite {
             for (WebElement details : elementDetails) {
                 String detailsText = details.getText();
 
-                if(StringUtils.isNotEmpty(detailsText)) {
-                    if(detailsText.contains("\n")){
+                if (StringUtils.isNotEmpty(detailsText)) {
+                    if (detailsText.contains("\n")) {
                         String[] splitDetails = detailsText.split("\\n");
 
-                        for(int i = 0; i < splitDetails.length; i++){
+                        for (int i = 0; i < splitDetails.length; i++) {
                             String detail = splitDetails[i];
                             stringBuilder.append(detail.trim()).append(" ");
 
-                            if(detail.contains("Length")) {
+                            if (detail.contains("Length")) {
                                 try {
-                                    String length = ScraperUtils.cleanNumber(splitDetails[i+1]);
+                                    String length = ScraperUtils.cleanNumber(splitDetails[i + 1]);
                                     boat.setLength(Integer.parseInt(length));
                                     stringBuilder.append(length).append(" ");
                                     i++;
